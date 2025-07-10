1ff0bf8c25d6ea1a58e0ddbf637fe09df408e358
Danilo Volles <danilo.volles@outlook.com>
2025-07-09 21:52:12 -0300

    feat: minimal documentation

--- src/main/java/com/danilo/volles/documents/api/controller/DocumentController.java
+++ src/main/java/com/danilo/volles/documents/api/controller/DocumentController.java
@@ -2,6 +2,10 @@ package com.danilo.volles.documents.api.controller;
 
 import com.danilo.volles.documents.api.dto.ApiResponseWrapper;
 import com.danilo.volles.documents.api.entity.Document;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Documents", description = "Documents endpoints")
 @RestController
 @RequestMapping("/documents")
 @RequiredArgsConstructor
@@ -16,6 +21,13 @@ public class DocumentController {
 
     private final DocumentProducer producer;
 
+    @Operation(
+            summary = "Send document's metadata",
+            description = "Send a document to KAFKA's Queue")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Document successfully sent"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping("/send")
     public ResponseEntity<ApiResponseWrapper> sendDocument(@RequestBody Document document) {
         producer.sendDocument(document);
--- src/main/java/com/danilo/volles/documents/api/entity/Document.java
+++ src/main/java/com/danilo/volles/documents/api/entity/Document.java
@@ -1,5 +1,6 @@
 package com.danilo.volles.documents.api.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Demo document object")
 public class Document implements Serializable {
     private String title;
     private String author;
